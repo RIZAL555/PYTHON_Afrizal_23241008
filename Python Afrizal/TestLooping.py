91b2d2f8f1e8a8d4a165dc5325b3f7f3cc56c6f7
RIZAL555 <147357342+RIZAL555@users.noreply.github.com>
2024-07-02 08:48:21 +0700

    Add files via upload

---
+++ Python Afrizal/TestLooping.py
@@ -0,0 +1,9 @@
+print('selamat datang guyyss')
+# jadi kita disini akan membuat program looping
+tanya = input('apakah anda muslim?: ')
+if tanya == 'iya':
+    for tanya in range(1, 10):
+        print('masyaallahh')
+else:
+       print('astagfirullahh')
+
